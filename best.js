8e993f0a59cdc4f82c2da6c0d416831860aca816
sammie <Alphasam123@gmail.com>
2022-01-18 17:01:58 +0000

    update to allow for more intelligent guesses

--- best.js
+++ best.js
@@ -1,105 +1,143 @@
 const fs = require('fs');
 
-//[undefined, undefined, undefined, undefined, undefined]
-const greens = [undefined, undefined, undefined, undefined, undefined];
-//[[], [], [], [], []]
-const yellows = [[], [], [], [], []];
-//[]
-const blacks = [];
+const greens = 'p-o-y'.split('');
+const yellows = [
+    ''.split(''),
+    ''.split(''),
+    'r'.split(''),
+    ''.split(''),
+    ''.split(''),
+];
+const blacks = 'taesin'.split('');
 
 try {
     /**
-     * @type {string[]} data
+     * @type {string[]} allWords
      */
-    const data = fs.readFileSync('words.txt', 'utf8').split('\r\n');
-    const validWords = data
-        .map((line) => line.toLowerCase())
-        .filter((word) => {
-            let isWordValid = true;
+    const allWords = fs
+        .readFileSync('words.txt', 'utf8')
+        .split('\r\n')
+        .map((line) => line.toLowerCase());
 
-            word.split('').forEach((letter, index) => {
-                if (greens[index] !== undefined && greens[index] !== letter)
-                    isWordValid = false;
-            });
+    const validWords = allWords.filter((word) => {
+        let isWordValid = true;
 
-            if (!isWordValid) return false;
-
-            yellows.forEach((list, i) => {
-                list.forEach((l) => {
-                    if (l === undefined) return;
-                    let doesWordContainYellows = false;
-
-                    word.split('').forEach((letter, index) => {
-                        if (letter === l) {
-                            if (index === i) {
-                                isWordValid = false;
-                            } else {
-                                doesWordContainYellows = true;
-                            }
-                        }
-                    });
-
-                    if (!doesWordContainYellows) isWordValid = false;
-                });
-            });
-
-            if (!isWordValid) return false;
-
-            blacks.forEach((l, i) => {
-                if (word.includes(l)) isWordValid = false;
-            });
-
-            return isWordValid;
+        word.split('').forEach((letter, index) => {
+            if (greens[index] !== '-' && greens[index] !== letter)
+                isWordValid = false;
         });
+
+        if (!isWordValid) return false;
+
+        yellows.forEach((list, i) => {
+            list.forEach((l) => {
+                if (l === undefined) return;
+                let doesWordContainYellows = false;
+
+                word.split('').forEach((letter, index) => {
+                    if (letter === l) {
+                        if (index === i) {
+                            isWordValid = false;
+                        } else {
+                            doesWordContainYellows = true;
+                        }
+                    }
+                });
+
+                if (!doesWordContainYellows) isWordValid = false;
+            });
+        });
+
+        if (!isWordValid) return false;
+
+        blacks.forEach((l, i) => {
+            if (word.includes(l)) isWordValid = false;
+        });
+
+        return isWordValid;
+    });
 
     console.log('found ' + validWords.length + ' valid 5 letter words');
 
-    const letterFrequency = {};
-    const letterPosFrequency = [{}, {}, {}, {}, {}];
+    if (validWords.length === 1) {
+        validWords.map((word) => console.log({ word }));
+    } else {
+        const letterFrequency = {};
+        const letterPosFrequency = [{}, {}, {}, {}, {}];
 
-    validWords.forEach((word) => {
-        const wordLetterFrequency = {};
+        validWords.forEach((word) => {
+            const wordLetterFrequency = {};
 
-        word.split('').forEach((letter, i) => {
-            wordLetterFrequency[letter] = 1;
-            letterPosFrequency[i][letter] =
-                (letterPosFrequency[i][letter] || 0) + 1;
+            word.split('').forEach((letter, i) => {
+                wordLetterFrequency[letter] = 1;
+                letterPosFrequency[i][letter] =
+                    (letterPosFrequency[i][letter] || 0) + 1;
+            });
+
+            Object.keys(wordLetterFrequency).forEach((letter) => {
+                letterFrequency[letter] = (letterFrequency[letter] || 0) + 1;
+            });
         });
 
-        Object.keys(wordLetterFrequency).forEach((letter) => {
-            letterFrequency[letter] = (letterFrequency[letter] || 0) + 1;
-        });
-    });
+        const calculateWordScore = (word) => {
+            const wordLetterScores = {};
 
-    const calculateWordScore = (word) => {
-        const wordLetterScore = {};
+            const posLetterScore = word.split('').reduce((score, letter, i) => {
+                wordLetterScores[letter] = letterFrequency[letter];
 
-        const posLetterScore = word.split('').reduce((score, letter, i) => {
-            wordLetterScore[letter] = letterFrequency[letter];
+                return score + letterPosFrequency[i][letter];
+            }, 0);
 
-            return score + letterPosFrequency[i][letter];
-        }, 0);
-
-        return (
-            Object.keys(wordLetterScore).reduce(
-                (a, b) => a + wordLetterScore[b],
+            const wordLetterScore = Object.keys(wordLetterScores).reduce(
+                (a, b) => a + wordLetterScores[b],
                 0
-            ) + posLetterScore
-        );
-    };
+            );
 
-    let wordScores = validWords.map((word) => ({
-        word,
-        score: calculateWordScore(word),
-    }));
+            return (
+                (isNaN(wordLetterScore) ? 0 : wordLetterScore) +
+                (isNaN(posLetterScore) ? 0 : posLetterScore)
+            );
+        };
 
-    wordScores.sort((a, b) => b.score - a.score);
-    wordScores = wordScores.map((word, i) => ({
-        ...word,
-        rank: i + 1,
-    }));
+        const calculateWordInformation = (word) => {
+            const wordLetterScores = {};
 
-    for (let i = 0; i < 5; i++) console.log(wordScores[i]);
+            const posLetterScore = word.split('').reduce((score, letter, i) => {
+                if (letter === greens[i]) return score;
+                if (blacks.includes(letter)) return score;
+                if (yellows.flat().includes(letter)) return score;
+
+                wordLetterScores[letter] = letterFrequency[letter];
+
+                return score + letterPosFrequency[i][letter];
+            }, 0);
+
+            const wordLetterScore = Object.keys(wordLetterScores).reduce(
+                (a, b) => a + wordLetterScores[b],
+                0
+            );
+
+            return (
+                (isNaN(wordLetterScore) ? 0 : wordLetterScore) +
+                (isNaN(posLetterScore) ? 0 : posLetterScore)
+            );
+        };
+
+        let wordScores = allWords.map((word) => ({
+            word,
+            score: calculateWordScore(word),
+            information: calculateWordInformation(word),
+        }));
+
+        wordScores.sort((a, b) => b.information - a.information);
+        wordScores = wordScores.map((word, i) => ({
+            ...word,
+            rank: i + 1,
+        }));
+
+        console.log('The top 5 words are:');
+        for (let i = 0; i < 5; i++) console.log(wordScores[i]);
+    }
 } catch (err) {
     console.error(err);
 }
